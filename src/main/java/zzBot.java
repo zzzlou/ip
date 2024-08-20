548bbfd1cd2fd0eb0b784e31465fbd40a1cf91cd
zzzlou <zhanzhilou@gmail.com>
2024-08-20 15:12:37 +0800

    added echo

--- src/main/java/zzBot.java
+++ src/main/java/zzBot.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class zzBot {
 
     public String name;
@@ -11,14 +12,31 @@ public class zzBot {
     public void greet() {
 
         String message = this.line + String.format("\nHello! I'm %s",this.name) +
-                "\nWhat can I do for you?\n" + this.line +
-                "\nBye. Hope to see you again soon!\n" + this.line;
+                "\nWhat can I do for you?\n" + this.line;
 
         System.out.println(message);
     }
 
+    public void bye() {
+        System.out.println(this.line + "\nBye. Hope to see you again soon!\n" + this.line);
+    }
+
+    public void echo(String input) {
+        System.out.println(input);
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         zzBot bot = new zzBot();
         bot.greet();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                bot.bye();
+                break;
+            } else {
+                bot.echo(input);
+            }
+        }
     }
 }

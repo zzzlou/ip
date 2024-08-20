dd1e5780ecfc62c9b57d33c21d8daa3e14301510
zzzlou <zhanzhilou@gmail.com>
2024-08-20 15:43:17 +0800

    added add and list

--- src/main/java/zzBot.java
+++ src/main/java/zzBot.java
@@ -1,17 +1,21 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class zzBot {
 
-    public String name;
-    public String line;
+    private String name;
+    private String line;
+    private ArrayList<String> botList;
 
     public zzBot() {
         this.name = "zzBot";
         this.line = "____________________________________________________________";
+        this.botList = new ArrayList<>();
     }
 
     public void greet() {
 
-        String message = this.line + String.format("\nHello! I'm %s",this.name) +
+        String message = this.line + String.format("\nHello! I'm %s", this.name) +
                 "\nWhat can I do for you?\n" + this.line;
 
         System.out.println(message);
@@ -25,6 +29,19 @@ public class zzBot {
         System.out.println(input);
     }
 
+    public void add(String string) {
+        botList.add(string);
+        System.out.println(String.format("added: %s",string));
+    }
+
+    public void list() {
+        int length = this.botList.size();
+        for (int i = 0; i < length; i++) {
+            String output = String.format("%d.  %s", i + 1, this.botList.get(i));
+            System.out.println(output);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         zzBot bot = new zzBot();
@@ -34,8 +51,10 @@ public class zzBot {
             if (input.equals("bye")) {
                 bot.bye();
                 break;
+            } else if (input.equals("list")) {
+                bot.list();
             } else {
-                bot.echo(input);
+                bot.add(input);
             }
         }
     }

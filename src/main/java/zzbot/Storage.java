c0043c3b92f35402212df6c7cbafad5a764fea50
zzzlou <zhanzhilou@gmail.com>
2024-08-29 11:12:55 +0800

    Add JavaDoc

--- src/main/java/zzbot/Storage.java
+++ src/main/java/zzbot/Storage.java
@@ -1,4 +1,5 @@
 package zzbot;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -7,57 +8,84 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * The {@code Storage} class handles reading from and writing to a file,
+ * storing and loading {@code Task} objects for persistence.
+ */
 public class Storage {
 
     private String path;
 
+    /**
+     * Constructs a {@code Storage} object with the specified file path.
+     *
+     * @param path The path of the file for storage.
+     */
     Storage(String path) {
         this.path = path;
     }
 
+    /**
+     * Records the tasks from the given {@code TaskList} to the file.
+     *
+     * @param tasks The {@code TaskList} containing tasks to record.
+     * @throws IOException If an I/O error occurs.
+     */
     public void record(TaskList tasks) throws IOException {
-        FileWriter fw = new FileWriter(this.path);
-        for (int i = 0; i < tasks.size(); i++) {
-            Task task = tasks.get(i);
-            String text = task.writeFile();
-            fw.write(text);
-        }
-        fw.close();
-    }
-
-    public ArrayList<Task> load() throws FileNotFoundException {
-        File f = new File(this.path);
-        if (!f.exists()) {
-            System.out.println("no");
-        }
-        Scanner s = new Scanner(f);
-        ArrayList<Task> arr = new ArrayList<>();
-        while (s.hasNext()) {
-            
-            String line = s.nextLine();
-            String[] ss = line.split("\\|");
-            String type = ss[0];
-            boolean isDone = ss[1] == "1" ? true:false;
-            String name = ss[2];
-            if (type.equals("D")) {
-                String deadline = ss[3];
-                LocalDate date = LocalDate.parse(deadline);
-                Task task = new Deadlines(name, isDone,date);
-                arr.add(task);
-            } else if (type.equals("E")) {
-                String start = ss[3];
-                String end = ss[4];
-                Task task = new Events(name, isDone,start, end);
-                arr.add(task);
-            } else {
-                Task task = new ToDos(name,isDone);
-                arr.add(task);
+        try (FileWriter fw = new FileWriter(this.path)) {
+            for (Task task : tasks) {
+                fw.write(task.writeFile());
             }
         }
-        s.close();
-
-        return arr;
-
     }
-    
+
+    /**
+     * Loads tasks from the file into an {@code ArrayList} of {@code Task}.
+     *
+     * @return An {@code ArrayList} containing the loaded tasks.
+     * @throws FileNotFoundException If the file is not found.
+     */
+    public ArrayList<Task> load() throws FileNotFoundException {
+        ArrayList<Task> tasks = new ArrayList<>();
+        File file = new File(this.path);
+
+        if (!file.exists()) {
+            System.out.println("File not found");
+            return tasks;
+        }
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                String[] parts = scanner.nextLine().split("\\|");
+                Task task = createTaskFromData(parts);
+                tasks.add(task);
+            }
+        }
+
+        return tasks;
+    }
+
+    /**
+     * Creates a task based on the given data array.
+     *
+     * @param data The string array containing task data.
+     * @return The created {@code Task} object.
+     */
+    private Task createTaskFromData(String[] data) {
+        String type = data[0];
+        boolean isDone = "1".equals(data[1]);
+        String name = data[2];
+
+        switch (type) {
+            case "D":
+                LocalDate deadline = LocalDate.parse(data[3]);
+                return new Deadlines(name, isDone, deadline);
+            case "E":
+                String start = data[3];
+                String end = data[4];
+                return new Events(name, isDone, start, end);
+            default:
+                return new ToDos(name, isDone);
+        }
+    }
 }
--- src/main/java/zzbot/ToDos.java
+++ src/main/java/zzbot/ToDos.java
@@ -1,23 +1,50 @@
 package zzbot;
-public class ToDos extends Task{
 
+/**
+ * The {@code ToDos} class represents a simple to-do task.
+ * It extends the {@code Task} class and provides specific implementations
+ * for describing the task and formatting it for file storage.
+ */
+public class ToDos extends Task {
+
+    /**
+     * Constructs a new {@code ToDos} task with the specified name.
+     * The task is initialized as not done.
+     *
+     * @param name The name of the to-do task.
+     */
     ToDos(String name) {
-        super(name,"T");
+        super(name, "T");
     }
 
+    /**
+     * Constructs a new {@code ToDos} task with the specified name and completion status.
+     *
+     * @param name   The name of the to-do task.
+     * @param isDone The completion status of the task (true if done, false otherwise).
+     */
     ToDos(String name, boolean isDone) {
-        super(name,isDone, "T");
+        super(name, isDone, "T");
     }
 
+    /**
+     * Provides a string representation of the to-do task, including its type, status, and name.
+     *
+     * @return A string describing the to-do task in the format: "[T][status] name".
+     */
     @Override
     public String describe() {
-        return String.format("[%s][%s] %s",
-                this.type, this.getStatusIcon(), this.getName());
+        return String.format("[%s][%s] %s", this.type, this.getStatusIcon(), this.getName());
     }
 
+    /**
+     * Formats the to-do task information for writing to a file.
+     * The output is formatted as: "T|isDone|name\n".
+     *
+     * @return A string formatted for file writing, representing the to-do task.
+     */
     @Override
     public String writeFile() {
-        return this.type + "|" + (this.isDone ? "1":"0") + "|" + this.name + "\n";
+        return this.type + "|" + (this.isDone ? "1" : "0") + "|" + this.name + "\n";
     }
-
 }

5e2344c7d4a7567a1bb79ac65b16429c08f8d94d
zzzlou <zhanzhilou@gmail.com>
2024-09-10 22:26:10 +0800

    Add help page

--- src/main/java/zzbot/ZzBotException.java
+++ src/main/java/zzbot/ZzBotException.java
@@ -3,6 +3,6 @@ package zzbot;
 public class ZzBotException extends Exception {
 
     public ZzBotException(String message) {
-        super("Oops, Duke did not work as expected because: " + message);
+        super("Oops, Duke did not work as expected " + message + "\nEnter 'help' for manual");
     }
 }
